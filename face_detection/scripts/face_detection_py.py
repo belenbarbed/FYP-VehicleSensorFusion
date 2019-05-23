fa03500713fa088e12888ee27fa679a40f9027ae
Belen Barbed <belenbarbed@gmail.com>
2019-05-23 11:02:38 +0100

    python3 face detection working great with 4 feeds

--- face_detection/scripts/face_detection_py.py
+++ face_detection/scripts/face_detection_py.py
@@ -5,18 +5,23 @@ from cv_bridge import CvBridge
 import face_recognition
 import numpy as np
 import rospy
+import time
 
 from sensor_msgs.msg import Image
 from face_detection.msg import Detected_Img
 
 # Detect faces on 1-in-freq frames
-freq = 5
+freq = 2
 counters = [freq, freq, freq, freq]
+
+# Scale frame down for speed
+scale = 2
 
 def callback(data, args):
     global br
     global counters
     global freq
+    global scale
     pixel_no = int(args[0])
     pub = args[1]
     rospy.loginfo("in callback for pixel " + str(pixel_no+1))
@@ -26,28 +31,30 @@ def callback(data, args):
     if(counters[pixel_no] <= 0):
         counters[pixel_no] = freq
 
+        # Timing
+        start =  time.clock()
+
         # Reduce frame size by half for speed purposes
         frame = br.imgmsg_to_cv2(data, desired_encoding="bgr8")
-        small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        small_frame = cv2.resize(frame, (0, 0), fx=1/scale, fy=1/scale)
         rgb_small_frame = small_frame[:, :, ::-1]
 
         # Detect faces in frame
         face_locations = face_recognition.face_locations(rgb_small_frame)
-        # face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         labels = []
         bboxes = []
         for face_location in face_locations:
-            rospy.loginfo("found a face at " + str(face_location))
+            # rospy.loginfo("found a face at " + str(face_location))
             labels.append("A face")
 
         # Display the results
         for (top, right, bottom, left), name in zip(face_locations, labels):
             # Scale back up face locations since the frame we detected in was scaled to 1/2 size
-            top *= 2
-            right *= 2
-            bottom *= 2
-            left *= 2
+            top *= scale
+            right *= scale
+            bottom *= scale
+            left *= scale
 
             # Draw a box around the face
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -58,12 +65,20 @@ def callback(data, args):
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         # Display the resulting image
-        cv2.imshow('Video', frame)
+        cv2.imshow('Video' + str(pixel_no+1), frame)
         cv2.waitKey(1)
-        # cv2.imwrite('/home/soteris-group/bb2115/frame.png',frame)
 
+        # Publish results for visualisation node
         msg = Detected_Img()
-        rospy.loginfo("detected faces in pixel " + str(pixel_no+1))
+        msg.header  = data.header
+        msg.img = frame
+        # msg.bboxes = ?
+        msg.labels = labels
+        pub.publish(msg)
+
+        # Timing
+        total =  time.clock() - start
+        rospy.loginfo("face detection took " + str(total) + " seconds")
 
 def main():
     rospy.loginfo("Initialising node...")
